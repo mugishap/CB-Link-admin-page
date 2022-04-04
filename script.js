2c00f62ab5e35c5774bc6eedb64b7dce74351e7e
precieux33 <precieuxmugisha@gmail.com>
2022-04-04 18:59:06 +0200

    overflow

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 function changeTheme() {
     let body = document.querySelector("body")
-    if(body) console.log("body is defined");
+    if (body) console.log("body is defined");
     else console.log("body is not defined");
     body.style.backgroundColor = "black"
-    console.log("fasdasf"); 
+    console.log("fasdasf");
 }
 function calculatorOff() {
-    container.style.display="none"
+    container.style.display = "none"
 }
 
 let calculatorBtn = document.querySelector(".calc-btn")
 let container = document.querySelector(".container")
-if(calculatorBtn){
-    calculatorBtn.addEventListener('click',()=>{
-        container.style.display="grid"
+if (calculatorBtn) {
+    calculatorBtn.addEventListener('click', () => {
+        container.style.display = "grid"
     })
 }
-else{
+else {
     console.log("Button is not defined");
+}
+function showNav() {
+    let icon = document.querySelector(".menu")
+    let list = document.querySelector(".on-small-screen")
+    if(list.style.display == "flex"){
+        list.style.display = "none"
+        icon.innerHTML = "menu"
+    }
+    else{
+        list.style.display = "flex"
+        icon.innerHTML = "close"
+    }
+    
+}
+if(document.documentElement.clientHeight == 693){
+
 }
